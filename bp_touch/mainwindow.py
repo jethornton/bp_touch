a04b39db40f969b7d8cc5b0065baea051e3ba632
John Thornton <jthornton@gnipsel.com>
2019-02-04 05:51:21 -0600

    add mdi and too info

--- bp_touch/mainwindow.py
+++ bp_touch/mainwindow.py
@@ -10,6 +10,9 @@ class MyMainWindow(VCPMainWindow):
         super(MyMainWindow, self).__init__(*args, **kwargs)
 
         self.offsetButtonGroup.buttonClicked.connect(self.offsetHandleKeys)
+        self.mdiButtonGroup.buttonClicked.connect(self.mdiHandleKeys)
+        self.mdiBackSpaceKey.clicked.connect(self.mdiBackSpace)
+        self.mdiClearKey.clicked.connect(self.mdiClear)
 
     def offsetHandleKeys(self, button):
         char = str(button.text())
@@ -19,3 +22,20 @@ class MyMainWindow(VCPMainWindow):
         else:
             text = char
         self.offsetLabel.setText(text)
+
+    def mdiHandleKeys(self, button):
+        char = str(button.text())
+        text = self.mdiEntry.text() or '0'
+        if text != '0':
+            text += char
+        else:
+            text = char
+        self.mdiEntry.setText(text)
+
+    def mdiBackSpace(self):
+        if len(self.mdiEntry.text()) > 0:
+            text = self.mdiEntry.text()[:-1]
+            self.mdiEntry.setText(text)
+
+    def mdiClear(self):
+        self.mdiEntry.setText('')
